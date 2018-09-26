f966164eca6a087fb88e98df7bbca900fb1970b3
kimdaeman14 <kimdaeman14@gmail.com>
2018-09-27 03:29:17 +0900

    UI구현완료

---
+++ SearchApp/SearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchApp
+//
+//  Created by kimdaeman14 on 2018. 9. 27..
+//  Copyright © 2018년 A3LCamel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
